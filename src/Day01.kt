0e540f2ee483488e782b4a307811f29281eb5695
Tristan Rothman <tristan@labs.epiuse.com>
2023-12-01 13:49:00 +0200

    Solve Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,52 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf { line ->
+            line.filter { it.isDigit() }.toCharArray().let { "${it.first()}${it.last()}".toInt() }
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return part1(
+            input.map { line ->
+                buildString {
+                    line.forEachIndexed { index, c ->
+                        if (c.isDigit()) {
+                            append(c)
+                        } else {
+                            Digit.entries.forEach { digit ->
+                                if (line.substring(index).startsWith(digit.name.lowercase())) append(digit.num)
+                            }
+                        }
+
+                    }
+                }
+            }
+        )
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput1 = readInput("Day01_Part01_test")
+    check(part1(testInput1) == 142)
 
     val input = readInput("Day01")
     part1(input).println()
+
+    val testInput2 = readInput("Day01_Part02_test")
+    check(part2(testInput2) == 281)
+
     part2(input).println()
+}
+
+enum class Digit(
+    val num: Int
+) {
+    ONE(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
 }
