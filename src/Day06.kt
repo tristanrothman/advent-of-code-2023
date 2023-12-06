9aa8198ac74bdbceebfe2cea396c79266bf530f1
Tristan Rothman <tristan@labs.epiuse.com>
2023-12-06 11:17:57 +0200

    Solve Day06

---
+++ src/Day06.kt
@@ -0,0 +1,33 @@
+fun main() {
+    fun ways(map: Map<Long, Long>): Long {
+        return map.entries.mapIndexed {index: Int, (time: Long, distance: Long) ->
+            (1L ..< time).count {
+                it * (time - it) > distance
+            }
+        }.reduce(Math::multiplyExact).toLong()
+
+    }
+
+    fun part1(input: List<String>): Long {
+        val (times, distances) = input.map { line ->
+            line.split(Regex("\\s+")).drop(1).map { it.toLong() }
+        }
+        return ways(times.zip(distances).toMap())
+    }
+
+    fun part2(input: List<String>): Long {
+        val (time, distance) = input.map { line ->
+            line.split(Regex("\\s+")).drop(1).joinToString("").toLong()
+        }
+        return ways(mapOf( time to distance))
+    }
+
+    val testInput = readInput("Day06_test")
+    val input = readInput("Day06")
+
+    check(part1(testInput) == 288L)
+    println(part1(input))
+
+    check(part2(testInput) == 71503L)
+    println(part2(input))
+}
